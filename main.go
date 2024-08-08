4eddbe83429a5f2afcf48337f3820bb36fa1f4f3
v.golikov <v.golikov@novostroy-m.ru>
2024-08-08 20:44:32 +0300

    try to parse form terminal

--- main.go
+++ main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"bufio"
 	"flag"
 	"fmt"
 	"math"
+	"os"
+	"strconv"
+	"strings"
 	"time"
 )
 
@@ -27,17 +31,92 @@ type Result struct {
 func init() {
 	flag.Parse()
 
-	fmt.Printf("Parsing flags:\nPerecent = %v\nStart Sum = %v\nMonth Income = %v\n", *Percent, *StartSum, *MonthIncome)
+	// if start from GO RUN with flags -percent=<> -startSum=<> -income=<>
+	if *Percent != 0 || *StartSum != 0 || *MonthIncome != 0 {
+		fmt.Printf("Parsing flags:\nPerecent = %v\nStart Sum = %v\nMonth Income = %v\n", *Percent, *StartSum, *MonthIncome)
+
+		// Process(Params{
+		// 	Percent:     *Percent / 1000,
+		// 	Sum:         *StartSum,
+		// 	MonthIncome: *MonthIncome,
+		// })
+	} else {
+		fmt.Println("Type -percent=<> -start=<> -month-income=<>\nType -start to start process")
+	}
 }
 
 func main() {
 	p := Params{
-		Percent:     *Percent / 1000,
-		Sum:         *StartSum,
-		MonthIncome: *MonthIncome,
+		Percent:     0,
+		Sum:         0,
+		MonthIncome: 0,
 	}
 
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		text := strings.TrimSpace(scanner.Text())
+		flags := strings.Split(text, "-")
+
+		for _, v := range flags {
+			flag := (strings.Split(v, "="))
+			if strings.ContainsAny(flag[0], "percent") {
+				flagValue := ""
+
+				if len(flag) == 2 {
+					flagValue = strings.TrimSpace(flag[1])
+				}
+
+				value, _ := strconv.ParseFloat(flagValue, 64)
+
+				// p.Percent = value
+				fmt.Println(value)
+			}
+
+			if strings.ContainsAny(flag[0], "start") {
+				flagValue := ""
+
+				if len(flag) == 2 {
+					flagValue = strings.TrimSpace(flag[1])
+				}
+
+				value, _ := strconv.ParseFloat(flagValue, 64)
+
+				// p.Sum = value
+				fmt.Println(value)
+			}
+
+			if strings.ContainsAny(flag[0], "income") {
+				flagValue := ""
+
+				if len(flag) == 2 {
+					flagValue = strings.TrimSpace(flag[1])
+				}
+
+				value, _ := strconv.ParseFloat(flagValue, 64)
+
+				// p.MonthIncome = value
+				fmt.Println(value)
+			}
+		}
+
+		if text == "start" {
+			fmt.Println(p)
+		}
+	}
+}
+
+// func Parse() (bool, string) {
+// 	if *StartSum == 0 {
+// 		return false, "Start Sum not entered"
+// 	}
+
+// 	return true, ""
+// }
+
+func Process(p Params) {
+	// fmt.Println(p)
 	var result []Result
+
 	for i := 0; i < 12; i++ {
 		percentIncome := math.Round(p.Sum * p.Percent)
 
